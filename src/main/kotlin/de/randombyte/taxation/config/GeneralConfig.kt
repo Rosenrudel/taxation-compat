d632026b6d8fd038752a3bd06bc16ae20fec30da
Timm Ortloff <timm.ortloff@icinga.com>
2022-10-07 00:52:55 +0200

    Fix Map not being parsed

--- src/main/kotlin/de/randombyte/taxation/config/GeneralConfig.kt
+++ src/main/kotlin/de/randombyte/taxation/config/GeneralConfig.kt
@@ -4,16 +4,12 @@ import ninja.leaping.configurate.objectmapping.Setting
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable
 
 @ConfigSerializable
-class GeneralConfig (
-        @Setting("session-duration") val sessionDuration: String = "72h",
-        @Setting("currency") val currency: String = "economylite:coin",
-        @Setting("virtual-tax-account") val taxAccount: String = "<put your virtual account in here>",
-        @Setting("taxation-rates") val rates: Map<Int, Double> = mapOf(
-                1_000 to 1.5,
-                10_000 to 3.5,
-                100_000 to 5.0
-        ),
+class GeneralConfig(
+    @Setting("session-duration") val sessionDuration: String = "72h",
+    @Setting("currency") val currency: String = "economylite:coin",
+    @Setting("virtual-tax-account") val taxAccount: String = "<put your virtual account in here>",
+    @Setting("taxation-rates") val rates: Map<Int, Double> = emptyMap(),
 
-        @Setting("session-end-commands", comment = "Commands that should be run when a session has stopped.")
-        val endTriggerCommands: List<String> = emptyList()
+    @Setting("session-end-commands", comment = "Commands that should be run when a session has stopped.")
+    val endTriggerCommands: List<String> = emptyList()
 )
