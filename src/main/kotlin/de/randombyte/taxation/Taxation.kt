4a93467319da1a88f74f0c702b56ccf201c5c7e3
Timm Ortloff <timm.ortloff@icinga.com>
2022-10-07 17:50:33 +0200

    Remove SettingDB methods and add DB Setup

--- src/main/kotlin/de/randombyte/taxation/Taxation.kt
+++ src/main/kotlin/de/randombyte/taxation/Taxation.kt
@@ -3,7 +3,6 @@ package de.randombyte.taxation
 import com.google.inject.Inject
 import de.randombyte.kosp.extensions.green
 import de.randombyte.kosp.extensions.toText
-import de.randombyte.taxation.Taxation.Companion.AUTHOR
 import de.randombyte.taxation.Taxation.Companion.ID
 import de.randombyte.taxation.Taxation.Companion.NAME
 import de.randombyte.taxation.Taxation.Companion.NUCLEUS_ID
@@ -11,7 +10,6 @@ import de.randombyte.taxation.Taxation.Companion.VERSION
 import de.randombyte.taxation.commands.ClearSessionCommand
 import de.randombyte.taxation.commands.SessionInfoCommand
 import de.randombyte.taxation.config.ConfigAccessor
-import de.randombyte.taxation.config.PersistenceDatabase
 import org.slf4j.Logger
 import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.CommandResult
@@ -25,9 +23,9 @@ import org.spongepowered.api.event.economy.EconomyTransactionEvent
 import org.spongepowered.api.event.filter.Getter
 import org.spongepowered.api.event.game.GameReloadEvent
 import org.spongepowered.api.event.game.state.GameInitializationEvent
-import org.spongepowered.api.event.game.state.GameStoppingServerEvent
 import org.spongepowered.api.plugin.Dependency
 import org.spongepowered.api.plugin.Plugin
+import org.spongepowered.api.scheduler.Task
 import org.spongepowered.api.service.economy.account.UniqueAccount
 import org.spongepowered.api.service.economy.transaction.ResultType
 import org.spongepowered.api.service.economy.transaction.TransactionResult
@@ -35,21 +33,24 @@ import org.spongepowered.api.service.economy.transaction.TransactionTypes.*
 import java.nio.file.Path
 import java.text.DecimalFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
-@Plugin(id = ID,
-        name = NAME,
-        version = VERSION,
-        authors = [AUTHOR, "TAINCER"],
-        dependencies = [(Dependency(id = NUCLEUS_ID, optional = true))])
+
+@Plugin(
+    id = ID,
+    name = NAME,
+    version = VERSION,
+    authors = ["RandomByte", "TAINCER"],
+    dependencies = [(Dependency(id = NUCLEUS_ID, optional = true))]
+)
 class Taxation @Inject constructor(
-        val logger: Logger,
-        @ConfigDir(sharedRoot = false) configPath: Path
+    val logger: Logger,
+    @ConfigDir(sharedRoot = false) configPath: Path
 ) {
     companion object {
         const val ID = "taxation"
         const val NAME = "Taxation"
         const val VERSION = "1.3.0"
-        const val AUTHOR = "RandomByte"
 
         const val NUCLEUS_ID = "nucleus"
 
@@ -64,17 +65,24 @@ class Taxation @Inject constructor(
         val DECIMAL_FORMATTER = DecimalFormat("0.00")
     }
 
-    val cause = Cause.of(EventContext.empty(), this)
+    val cause: Cause = Cause.of(EventContext.empty(), this)
 
     val configAccessor = ConfigAccessor(configPath)
 
-    var session: Session? = null
+    var database: Database? = null
 
     @Listener
     fun onInit(event: GameInitializationEvent) {
         configAccessor.reloadAll()
-        restoreSessionFromPersistence()
         registerCommands()
+
+        Task.builder()
+            .interval(1, TimeUnit.HOURS)
+            .execute { -> Session().checkTaxCollection() }
+            .submit(this)
+
+        database = Database()
+        database!!.createTables()
 
         logger.info("Loaded $NAME: $VERSION")
     }
@@ -87,21 +95,14 @@ class Taxation @Inject constructor(
     }
 
     @Listener
-    fun onShutdown(event: GameStoppingServerEvent) {
-        if (session?.isRunning == true) {
-            val persistentConfig = session!!.toPersistentConfig()
-            configAccessor.persistenceDatabase.save(persistentConfig)
-            logger.info("Session saved with remaining time: ${persistentConfig.sessionRemainingTime}")
-        }
-    }
-
-    @Listener
-    fun onMoneyTransaction(event: EconomyTransactionEvent, @Getter("getTransactionResult") transactionResult: TransactionResult) {
+    fun onMoneyTransaction(
+        event: EconomyTransactionEvent,
+        @Getter("getTransactionResult") transactionResult: TransactionResult
+    ) {
         if (
-                session?.isRunning == true &&
-                transactionResult.account is UniqueAccount &&
-                transactionResult.result == ResultType.SUCCESS &&
-                transactionResult.currency.id == currency.id
+            transactionResult.account is UniqueAccount &&
+            transactionResult.result == ResultType.SUCCESS &&
+            transactionResult.currency.id == currency.id
         ) {
             val previousBalance = transactionResult.type.let { type ->
                 val reversedOperation = when (type) {
@@ -112,73 +113,38 @@ class Taxation @Inject constructor(
 
                 transactionResult.account.getBalance(currency) + reversedOperation
             }
-            session!!.trackAccount((transactionResult.account as UniqueAccount).uniqueId, previousBalance)
+            database!!.maybeTrackPlayer(transactionResult.account as UniqueAccount, previousBalance)
         }
     }
 
     private fun registerCommands() {
         val sessionInfoCommand = CommandSpec.builder()
-                .permission("$ROOT_PERMISSION.session.info.self")
-                .arguments(optional(requiringPermission(user(USER_ARG), "$ROOT_PERMISSION.session.info.others")))
-                .executor(SessionInfoCommand())
-                .build()
+            .permission("$ROOT_PERMISSION.session.info.self")
+            .arguments(optional(requiringPermission(user(USER_ARG), "$ROOT_PERMISSION.session.info.others")))
+            .executor(SessionInfoCommand())
+            .build()
 
         Sponge.getCommandManager().register(this, CommandSpec.builder()
+            .child(CommandSpec.builder()
+                .child(
+                    CommandSpec.builder()
+                        .permission("$ROOT_PERMISSION.session.clean")
+                        .arguments(user(USER_ARG.toText()))
+                        .executor(ClearSessionCommand())
+                        .build(), "clear"
+                )
                 .child(CommandSpec.builder()
-                        .child(CommandSpec.builder()
-                                .permission("$ROOT_PERMISSION.session.clean")
-                                .arguments(user(USER_ARG.toText()))
-                                .executor(ClearSessionCommand())
-                                .build(), "clear")
-                        .child(CommandSpec.builder()
-                                .permission("$ROOT_PERMISSION.session.reset")
-                                .executor { src, _ ->
-                                    resetSession()
-                                    src.sendMessage("Reset session!".green())
-                                    return@executor CommandResult.success()
-                                }
-                                .build(), "reset")
-                        .child(sessionInfoCommand, "info")
-                        .build(), "session")
-                .build(), ID)
+                    .permission("$ROOT_PERMISSION.session.reset")
+                    .executor { src, _ ->
+                        database!!.emptyPlayers()
+                        src.sendMessage("Reset session!".green())
+                        return@executor CommandResult.success()
+                    }
+                    .build(), "reset")
+                .child(sessionInfoCommand, "info")
+                .build(), "session")
+            .build(), ID)
 
         Sponge.getCommandManager().register(this, sessionInfoCommand, "taxes")
-    }
-
-    fun resetSession() {
-        session?.stop(isForcefullyStopped = true)
-        session = Session(duration = generalConfig.sessionDuration.deserializeDuration())
-        session!!.start(plugin = this)
-    }
-
-    /**
-     * Restores saved data from the [PersistenceDatabase] or defaults to values set in the config
-     */
-    private fun restoreSessionFromPersistence() {
-        if (session?.isRunning == true) throw IllegalStateException("A session is already running!")
-
-        return
-
-        with(configAccessor.persistenceDatabase) {
-            val persistence = get()
-
-            val remainingDuration = (persistence.sessionRemainingTime ?: generalConfig.sessionDuration).deserializeDuration()
-            val displayDuration = persistence.sessionDuration?.deserializeDuration() ?: remainingDuration
-
-            val balances = if (persistence.previousBalances != null) {
-                persistence.previousBalances
-                        .mapValues { (_, serializedBigDecimal) -> serializedBigDecimal.toBigDecimal() }
-                        .toMutableMap()
-            } else mutableMapOf()
-
-            session = Session(
-                    duration = remainingDuration,
-                    originalTotalDuration = displayDuration,
-                    balances = balances)
-
-            session!!.start(this@Taxation)
-
-            save(PersistenceDatabase()) // save empty config, only write back to it when shutting down the server
-        }
     }
 }
