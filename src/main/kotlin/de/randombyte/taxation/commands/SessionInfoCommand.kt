7fb721ee7645af497f49d5527dcac92b7b32e2a3
Timm Ortloff <timm.ortloff@icinga.com>
2022-10-07 17:51:04 +0200

    Adjust SessionInfoCommand to use Database Data

--- src/main/kotlin/de/randombyte/taxation/commands/SessionInfoCommand.kt
+++ src/main/kotlin/de/randombyte/taxation/commands/SessionInfoCommand.kt
@@ -15,41 +15,45 @@ import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
 import org.spongepowered.api.command.spec.CommandExecutor
 import org.spongepowered.api.entity.living.player.User
-import java.math.BigDecimal
+import java.time.Duration
+import java.time.Instant
 
 class SessionInfoCommand : CommandExecutor {
     override fun execute(src: CommandSource, args: CommandContext): CommandResult {
-        if (Taxation.INSTANCE.session?.isRunning == true) {
-            val session = Taxation.INSTANCE.session!!
+        var target = args.getOne<User>(Taxation.USER_ARG).orNull()
+        if (target == null) {
+            if (src !is User) throw CommandException("Must be executed by a player or with a player as an argument!".toText())
+            target = src
+        } else {
+            src.sendMessage("Showing tax info for player ${target.name}.".yellow())
+        }
 
-            var target = args.getOne<User>(Taxation.USER_ARG).orNull()
-            if (target == null) {
-                if (src !is User) throw CommandException("Must be executed by a player or with a player as an argument!".toText())
-                target = src
-            } else {
-                src.sendMessage("Showing tax info for player ${target.name}.".yellow())
-            }
+        val session = Session()
 
-            val remainingDurationString = session.remainingDuration.serialize(outputMillis = false)
-            val income = session.calculateIncome(target.uniqueId)
-            val percentage = if (target.hasPermission(Taxation.TAX_EXEMPT_PERMISSION)) 0.0 else session.calculateTaxPercentage(income.toDouble())
-            val taxTotal = session.calculateTaxTotal(income.toDouble(), percentage)
-            val taxTotalHistory = statisticsDatabase.players[target.uniqueId]?.totalTaxPayed ?: BigDecimal.ZERO
+        val rma = Taxation.INSTANCE.database!!.getRuntimeValue("next_tax_collection_at")
+            ?.let { Duration.ofSeconds(it.toLong() - Instant.now().epochSecond) }
+        val remainingDurationString = rma?.serialize(outputMillis = false)
+        val income = session.calculateIncome(target.uniqueId)
+        val percentage =
+            if (target.hasPermission(Taxation.TAX_EXEMPT_PERMISSION)) 0.0 else session.calculateTaxPercentage(income.toDouble())
+        val taxTotal = session.calculateTaxTotal(income.toDouble(), percentage)
+        val taxTotalHistory = Taxation.INSTANCE.database!!.getPlayerStatistics(target.uniqueId)
 
-            texts.info.forEach { line ->
-                src.sendMessage(line
-                        .replace(
-                                REMAINING_DURATION to remainingDurationString,
-                                SESSION_INCOME to DECIMAL_FORMATTER.format(income),
-                                TAX_PERCENTAGE to DECIMAL_FORMATTER.format(percentage),
-                                TAX_TOTAL to DECIMAL_FORMATTER.format(taxTotal),
-                                TAX_HISTORY_TOTAL to DECIMAL_FORMATTER.format(taxTotalHistory),
-                                CURRENCY_SYMBOL to currency.symbol.serialize()
-                        )
-                        .tryReplacePlaceholders(source = target)
-                        .deserialize())
-            }
-        } else src.sendMessage("No session is running!".red())
+        texts.info.forEach { line ->
+            src.sendMessage(
+                line
+                    .replace(
+                        (REMAINING_DURATION to remainingDurationString) as Pair<String, String>,
+                        SESSION_INCOME to DECIMAL_FORMATTER.format(income),
+                        TAX_PERCENTAGE to DECIMAL_FORMATTER.format(percentage),
+                        TAX_TOTAL to DECIMAL_FORMATTER.format(taxTotal),
+                        TAX_HISTORY_TOTAL to DECIMAL_FORMATTER.format(taxTotalHistory),
+                        CURRENCY_SYMBOL to currency.symbol.serialize()
+                    )
+                    .tryReplacePlaceholders(source = target)
+                    .deserialize()
+            )
+        }
 
         return CommandResult.success()
     }
